2b443c3f2eea595145c8ae2dc9403def936f73dc
Jordan Barnes <jordanbarnes@macbook-air.mynetworksettings.com>
2024-07-17 16:56:52 -0400

    Added some changed to fix ios-url-launcher

--- heartland_photo_app/lib/home_screen.dart
+++ heartland_photo_app/lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:camera/camera.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:heartland_photo_app/annotation_screen.dart';
 import 'photo_screen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   final List<CameraDescription> cameras;
@@ -77,8 +78,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     }).toList(),
                   ),
                   TextField(
-                    decoration:
-                        const InputDecoration(labelText: 'Or create a new folder'),
+                    decoration: const InputDecoration(
+                        labelText: 'Or create a new folder'),
                     onChanged: (value) {
                       newFolder = value;
                     },
@@ -127,9 +128,45 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Heartland Workforce Solutions', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+        title: const Text('Heartland Workforce Solutions',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold)),
         backgroundColor: Colors.blueGrey[800],
         elevation: 0,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey[800],
+              ),
+              child: const Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.folder),
+              title: const Text('Manage/View Folders'),
+              onTap: () async {
+                final Uri uri =
+                    Uri.parse('https://photo-viewer-eight.vercel.app');
+                if (await canLaunch(uri.toString())) {
+                  await launch(uri.toString());
+                } else {
+                  throw 'Could not launch $uri';
+                }
+              },
+            ),
+          ],
+        ),
       ),
       body: RefreshIndicator(
         onRefresh: _refreshFolders,
@@ -179,8 +216,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         : 'Folder: $_selectedFolder'),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.orange,
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
@@ -193,8 +230,8 @@ class _HomeScreenState extends State<HomeScreen> {
                     label: const Text('Take Photo'),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.white,
-                      padding:
-                          const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 32, vertical: 16),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(30),
                       ),
